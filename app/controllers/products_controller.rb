bb9857c57cde121003df30914be1380692d07bd4
Patel Harshad <harshadce2008@gmail.com>
2016-12-23 16:54:56 +0530

    First file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,17 @@ class ProductsController < ApplicationController
     #This is for Shoppe
 
   def index
-    @products = Shoppe::Product.root.ordered.includes(:product_categories, :variants)
-    @products = @products.group_by(&:product_category)
+    if params[:amount]
+      @products = Shoppe::Product.root.where("price < #{params[:amount].to_s.tr('$', '').to_f}")
+      # .ordered.includes(:product_categories, :variants)
+      #@products = @products.group_by(&:product_category)
+    else
+      # @products = Shoppe::Product.root.ordered.includes(:product_categories, :variants)
+      @products = Shoppe::Product.root
+      # .where("price > 60")
+      # abort @products.inspect
+      # @products = @products.group_by(&:product_category)
+    end
   end
 
   def show
@@ -13,6 +22,7 @@ class ProductsController < ApplicationController
 
   def buy
   	@product = Shoppe::Product.root.find_by_permalink!(params[:permalink])
+    #abort @product.inspect
   	current_order.order_items.add_item(@product, 1)
   	redirect_to product_path(@product.permalink), :notice => "Product has been added successfuly!"
   end
